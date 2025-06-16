da8af294f232ab83f731616f729d4ff117291423
beno <benjamin@noessler.at>
2025-06-16 17:04:18 +0200

    linting fixes

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -58,6 +58,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   } catch (e) {
+    console.error(e);
     return {
       message: "Database Error: Failed to Create Invoice",
     };
@@ -98,6 +99,7 @@ export async function updateInvoice(
     WHERE id = ${id}
   `;
   } catch (e) {
+    console.error(e);
     return {
       message: "Database Error: Failed to Update Invoice",
     };
